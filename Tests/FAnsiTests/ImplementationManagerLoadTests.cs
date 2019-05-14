ee7f1a189c59ae99f08d3cc5e2c91a0bce2483cf
Thomas Nind <TZNind@dundee.ac.uk>
2019-05-14 11:09:08 +0100

    FAnsi Sql ImplementationManager no longer discards all old IImplementation when Loading new ones.  Added MEF nuget package to list of dependencies in nuspec.

---
+++ Tests/FAnsiTests/ImplementationManagerLoadTests.cs
@@ -0,0 +1,43 @@
+﻿using FAnsi.Implementation;
+using FAnsi.Implementations.MicrosoftSQL;
+using FAnsi.Implementations.MySql;
+using FAnsi.Implementations.Oracle;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FAnsiTests
+{
+    class ImplementationManagerLoadTests
+    {
+        [Test]
+        public void Test_LoadAssemblies_FromDirectory()
+        {
+            ImplementationManager.Clear();
+            Assert.IsNull(ImplementationManager.GetImplementations());
+            ImplementationManager.Load(new DirectoryInfo(TestContext.CurrentContext.TestDirectory));
+            Assert.GreaterOrEqual(ImplementationManager.GetImplementations().Count,3);
+        }
+        [Test]
+        public void Test_LoadAssemblies_OneAfterAnother()
+        {
+            ImplementationManager.Clear();
+            Assert.IsNull(ImplementationManager.GetImplementations());
+
+            ImplementationManager.Load<MicrosoftSQLImplementation>();
+            Assert.AreEqual(ImplementationManager.GetImplementations().Count,1);
+
+            ImplementationManager.Load<OracleImplementation>();
+            Assert.AreEqual(ImplementationManager.GetImplementations().Count,2);
+
+            //repeat loading shouldn't increase the count
+            ImplementationManager.Load<OracleImplementation>();
+            Assert.AreEqual(ImplementationManager.GetImplementations().Count,2);
+
+                        ImplementationManager.Load<MySqlImplementation>();
+            Assert.AreEqual(ImplementationManager.GetImplementations().Count,3);
+        }
+    }
+}
