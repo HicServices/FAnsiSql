3c6d672744fd8e2458c3f402584e82c9ee8232d5
James A Sutherland <j@sutherland.pw>
2022-02-03 12:10:16 +0000

    v1.0.8

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.7")]
-[assembly: AssemblyFileVersion("1.0.7")]
-[assembly: AssemblyInformationalVersion("1.0.7")]
+[assembly: AssemblyVersion("1.0.8")]
+[assembly: AssemblyFileVersion("1.0.8")]
+[assembly: AssemblyInformationalVersion("1.0.8")]
