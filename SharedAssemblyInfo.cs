8457c665f03863117f2d3ee6a4f4e2576ea5312c
James A Sutherland <j@sutherland.pw>
2024-02-14 08:57:50 -0600

    Update SharedAssemblyInfo.cs
    Add NeutralResourcesLanguage

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
 [assembly: AssemblyProduct("FAnsiSql")]
 [assembly: AssemblyCopyright("Copyright (c) 2018 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-GB")]
 
 [assembly: AssemblyVersion("3.2.0")]
 [assembly: AssemblyFileVersion("3.2.0")]
