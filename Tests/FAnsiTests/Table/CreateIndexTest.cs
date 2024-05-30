22e3d630a0e600e75ec1d6945c1ec2e815321ff6
James A Sutherland <j@sutherland.pw>
2024-05-30 15:43:30 -0500

    Fix up index creation tests and simplify

--- Tests/FAnsiTests/Table/CreateIndexTest.cs
+++ Tests/FAnsiTests/Table/CreateIndexTest.cs
@@ -12,8 +12,8 @@ namespace FAnsiTests.Table;
 
 internal sealed class CreateIndexTest : DatabaseTests
 {
-    [TestCaseSource(typeof(All), nameof(All.DatabaseTypes))]
-    public void TestBasicCase_IndexCreated(DatabaseType databaseType)
+    [TestCaseSource(typeof(All), nameof(All.DatabaseTypesWithTwoBoolFlags))]
+    public void TestBasicCase_IndexCreated(DatabaseType databaseType, bool unique, bool multiColumn)
     {
         // Force columns B and C to be strings otherwise Oracle gets upset by TypeGuesser mis-guessing the nulls as boolean
         var b = new DataColumn("B", typeof(string));
@@ -36,68 +36,10 @@ internal sealed class CreateIndexTest : DatabaseTests
             tbl = db.CreateTable("Fish", dt);
         }
 
-        var col = tbl.DiscoverColumn("A");
+        var colA = tbl.DiscoverColumn("A");
+        var colB = tbl.DiscoverColumn("B");
 
-        Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", [col]));
-        Assert.DoesNotThrow(() => tbl.DropIndex("my_index"));
-    }
-    public void TestBasicCase_IndexCreated_MultiColumn(DatabaseType databaseType)
-    {
-        // Force columns B and C to be strings otherwise Oracle gets upset by TypeGuesser mis-guessing the nulls as boolean
-        var b = new DataColumn("B", typeof(string));
-        b.SetDoNotReType(true);
-        var c = new DataColumn("C", typeof(string));
-        c.SetDoNotReType(true);
-        DiscoveredTable tbl;
-        using (var dt = new DataTable("Fish"))
-        {
-            dt.Columns.Add("A");
-            dt.Columns.Add(b);
-            dt.Columns.Add(c);
-
-            dt.Rows.Add("a1", null, null);
-            dt.Rows.Add("a2", null, null);
-            dt.Rows.Add("a3", null, null);
-
-            var db = GetTestDatabase(databaseType);
-
-            tbl = db.CreateTable("Fish", dt);
-        }
-
-        var col = tbl.DiscoverColumn("A");
-        var col2 = tbl.DiscoverColumn("B");
-
-        Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", [col, col2]));
-        Assert.DoesNotThrow(() => tbl.DropIndex("my_index"));
-    }
-
-    public void TestBasicCase_IndexCreated_Unique(DatabaseType databaseType)
-    {
-        // Force columns B and C to be strings otherwise Oracle gets upset by TypeGuesser mis-guessing the nulls as boolean
-        var b = new DataColumn("B", typeof(string));
-        b.SetDoNotReType(true);
-        var c = new DataColumn("C", typeof(string));
-        c.SetDoNotReType(true);
-        DiscoveredTable tbl;
-        using (var dt = new DataTable("Fish"))
-        {
-            dt.Columns.Add("A");
-            dt.Columns.Add(b);
-            dt.Columns.Add(c);
-
-            dt.Rows.Add("a1", null, null);
-            dt.Rows.Add("a2", null, null);
-            dt.Rows.Add("a3", null, null);
-
-            var db = GetTestDatabase(databaseType);
-
-            tbl = db.CreateTable("Fish", dt);
-        }
-
-        var col = tbl.DiscoverColumn("A");
-        var col2 = tbl.DiscoverColumn("B");
-
-        Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", [col, col2], true));
+        Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", multiColumn ? [colA,colB] : [colA], unique));
         Assert.DoesNotThrow(() => tbl.DropIndex("my_index"));
     }
 
