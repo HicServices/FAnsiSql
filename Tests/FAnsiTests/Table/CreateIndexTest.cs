3e030786662562300aa9733845d6838b562c3c4a
James Friel <JFriel001@dundee.ac.uk>
2024-05-30 10:21:59 +0100

    add multi-column-test

--- Tests/FAnsiTests/Table/CreateIndexTest.cs
+++ Tests/FAnsiTests/Table/CreateIndexTest.cs
@@ -41,6 +41,35 @@ internal sealed class CreateIndexTest: DatabaseTests
         Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", [col]));
         Assert.DoesNotThrow(() => tbl.DropIndex("my_index"));
     }
+    public void TestBasicCase_IndexCreated_MultiColumn(DatabaseType databaseType)
+    {
+        // Force columns B and C to be strings otherwise Oracle gets upset by TypeGuesser mis-guessing the nulls as boolean
+        var b = new DataColumn("B", typeof(string));
+        b.SetDoNotReType(true);
+        var c = new DataColumn("C", typeof(string));
+        c.SetDoNotReType(true);
+        DiscoveredTable tbl;
+        using (var dt = new DataTable("Fish"))
+        {
+            dt.Columns.Add("A");
+            dt.Columns.Add(b);
+            dt.Columns.Add(c);
+
+            dt.Rows.Add("a1", null, null);
+            dt.Rows.Add("a2", null, null);
+            dt.Rows.Add("a3", null, null);
+
+            var db = GetTestDatabase(databaseType);
+
+            tbl = db.CreateTable("Fish", dt);
+        }
+
+        var col = tbl.DiscoverColumn("A");
+        var col2 = tbl.DiscoverColumn("B");
+
+        Assert.DoesNotThrow(() => tbl.CreateIndex("my_index", [col,col2]));
+        Assert.DoesNotThrow(() => tbl.DropIndex("my_index"));
+    }
 
     [TestCaseSource(typeof(All), nameof(All.DatabaseTypes))]
     public void TestBasicCase_DeleteIndexThatDoesntExist(DatabaseType databaseType)
